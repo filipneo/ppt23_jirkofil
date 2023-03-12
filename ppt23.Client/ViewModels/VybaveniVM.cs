8dd1e36d8b081b19a085c6ee54bd2a5f33021fee
filipneo <81971814+filipneo@users.noreply.github.com>
2023-03-12 15:29:47 +0100

    vybaveni table

--- ppt23.Client/ViewModels/VybaveniVM.cs
+++ ppt23.Client/ViewModels/VybaveniVM.cs
@@ -1,34 +1,58 @@
-﻿namespace ppt23.Client.ViewModels;
+﻿using System.Collections.Generic;
+
+namespace ppt23.Client.ViewModels;
 
 public class VybaveniVM
 {
-    public string Name { get; set; } = "";
+    public string Name { get; set; }
     public DateTime BoughtDateTime { get; set; }
     public DateTime LastRevision { get; set; }
-    public bool IsRevisionNeeded;
+    public bool IsRevisionNeeded { get => LastRevision < DateTime.Now.AddYears(-2); }
 
-    public VybaveniVM(string name, DateTime boughtDateTime, DateTime lastRevision)
+    public VybaveniVM()
     {
-        Name = name;
-        BoughtDateTime = boughtDateTime;
-        LastRevision = lastRevision;
-        IsRevisionNeeded = IsRevisionOld(LastRevision);
+		Random random = new();
+        DateTime since = new DateTime(2013, 01, 01);
+
+		Name = GetRandomName(random.Next(4,10));
+        BoughtDateTime = GetRandomDate(since);
+        LastRevision = GetRandomDate(since);
 	}
 
-	public static bool IsRevisionOld(DateTime lastRevision)
-	{
-		DateTime currentDate = DateTime.Now;
+    public string GetRandomName(int len)
+    {
+        Random random = new();
+        const string letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
-		if (lastRevision < currentDate.AddYears(-2)) return true;
-
-		else return false;
+        return new string(Enumerable.Range(0, random.Next(len))
+            .Select(x => letters[random.Next(letters.Length)])
+            .ToArray());
 	}
 
-/*	
-	public static List<VybaveniVM> ReturnRandomList(int number)
+    public DateTime GetRandomDate(DateTime minDate)
     {
-        return new List<VybaveniVM> { new VybaveniVM() { Name = "" }};
+		Random random = new();
+
+		TimeSpan timeSpan = DateTime.Now - minDate;
+		TimeSpan randomSpan = new TimeSpan((long)(random.NextDouble() * timeSpan.Ticks));
+		DateTime randomDate = minDate + randomSpan;
+
+		return randomDate;
+	}
+
+	public static List<VybaveniVM> ReturnRandomList()
+    {
+        Random random = new();
+
+        List<VybaveniVM> vybaveniList = new();
+
+        for (int i = 0; i < random.Next(5, 15); i++)
+        {
+            VybaveniVM vybaveni = new();
+            vybaveniList.Add(vybaveni);
+        }
+
+        return vybaveniList;
     }
-*/
 
 }
